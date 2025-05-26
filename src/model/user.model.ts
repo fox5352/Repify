ceafee4bb4a38acb3359ec1d3b25d1601dc10582
fox5352 <christophervos522@gmail.com>
2025-05-26 13:47:25 +0200

    added workoutroutine model

---
+++ src/model/user.model.ts
@@ -0,0 +1,133 @@
+import { db } from "./database";
+
+export interface UserAuthData {
+	id: string,
+	email?: string,
+	providers: string[],
+	hasImage: boolean,
+	imageUrl: string,
+}
+
+export interface UserTableData {
+	username: string,
+	bookmarksLength: number,
+	activeDays: number
+	lastActive: Date
+}
+
+export interface UserData extends UserAuthData, UserTableData { };
+
+/// user methods here
+export async function checkUserExists(id: string) {
+	const { data, error } = await db
+		.from('Users')
+		.select('id')
+		.eq('id', id)
+		.limit(1);
+
+	if (error) {
+		console.error('Error querying auth.users:', error);
+		return false;
+	}
+	return data.length > 0;
+}
+
+
+export async function createUser(id: string, userData: UserTableData): Promise<boolean> {
+	try {
+		// Check if already in Users
+		const { data } = await db
+			.from('Users')
+			.select('*');
+
+		if (data != null && data.length > 0) return false; // user exists
+
+		// Insert now
+		await db.from('Users').insert({
+			id,
+			username: userData.username,
+			activeDays: 0,
+			bookmarksLength: 0,
+		});
+
+		return true;
+	} catch (error) {
+		console.error('Insert error:', error);
+		return false;
+	}
+}
+
+export async function getUser(): Promise<UserData | null> {
+	const { data, error } = await db.auth.getSession();
+
+	if (!data) {
+		console.error(error)
+		return null;
+	}
+
+	if (data.session) {
+		if (data.session.user) {
+			const id = (await db.auth.getUser()).data.user?.id!;
+			const exists = await checkUserExists(id);
+
+			if (!exists) {
+				const name = data.session.user.user_metadata.user_name || data.session.user.user_metadata.full_name;
+				await createUser(
+					id
+					, {
+						username: name,
+						bookmarksLength: 0,
+						activeDays: 0,
+						lastActive: new Date()
+					});
+			}
+
+			const { data: users } = await db.from("Users").select("*").eq("id", id).limit(1);
+
+			if (users == null) return null;
+
+			const user: UserTableData & { id: string } = users[0];
+
+			return {
+				id: user.id,
+				email: data.session.user.email,
+				providers: data.session.user.app_metadata.providers,
+				//
+				hasImage: data.session.user.user_metadata.avatar_url.length > 0,
+				imageUrl: data.session.user.user_metadata.avatar_url,
+				//
+				username: user.username,
+				activeDays: user.activeDays,
+				bookmarksLength: user.bookmarksLength,
+				lastActive: user.lastActive
+			};
+		}
+	}
+
+	return null;
+}
+
+
+export async function IncrementDaysActive() {
+	try {
+		const id = (await db.auth.getUser()).data.user?.id!;
+		const exists = await checkUserExists(id);
+
+		if (!exists) return false;
+
+		const user = await getUser();
+
+		if (!user) return false;
+
+		/// TODO: add a last signd in data to table later and check based of of that
+
+		// await db.from("Users").update({
+		// 	activeDays: user.activeDays + 1
+		// });
+
+		return true;
+	} catch (error) {
+		console.error(error)
+		return false;
+	}
+}
---
+++ src/model/workoutroutine.model.ts
@@ -0,0 +1,103 @@
+import { db } from "./database.ts";
+import { getUser } from "./user.model.ts";
+
+interface DatabaseMetaData {
+	id: string,
+	created_at: string,
+	user_id: string
+}
+
+export type WorkoutType = {
+	index?: number,
+	sets: number,
+	name: string,
+	reps?: number, time?: number,
+	weight: number
+}
+
+export interface WorkoutRoutine {
+	title: string, workouts: WorkoutType[]
+}
+
+async function insertWorkoutSet(set: WorkoutType & { workout_routine_id: string }): Promise<boolean> {
+	try {
+		await db.from("WorkoutSets").insert({
+			workout_routine_id: set.workout_routine_id,
+			index: set.index,
+			name: set.name,
+			sets: set.sets,
+			weight: set.weight,
+			reps: set.reps || null,
+			time: set.reps || null
+		})
+		return true
+	} catch (error) {
+		console.error(error);
+		return false;
+	}
+}
+
+export async function uploadWorkoutRoutine(workoutRoutine: WorkoutRoutine): Promise<boolean> {
+	try {
+		const user = await getUser();
+
+		if (!user) return false;
+
+		const result = await db.from("WorkoutRoutines").insert({
+			user_id: user.id,
+			title: workoutRoutine.title
+		}).select();
+
+		if (!result.data) return false;
+
+		const headOfWorkout = result.data[0];
+
+		for (const set of workoutRoutine.workouts) {
+			await insertWorkoutSet({
+				...set,
+				workout_routine_id: headOfWorkout.id
+			});
+		}
+
+		return true;
+	} catch (error) {
+		console.error(error);
+		return false;
+	}
+}
+
+
+
+type getWorkoutRoutineType = WorkoutRoutine & DatabaseMetaData
+
+export async function getWorkoutRoutines(): Promise<getWorkoutRoutineType[] | null> {
+	try {
+		const user = await getUser();
+
+		if (!user) return [] as getWorkoutRoutineType[];
+
+
+		const { data, error } = await db
+			.from("WorkoutRoutines")
+			.select(`
+    *,
+    WorkoutSets(*)
+  `)
+			.eq("user_id", user.id);
+
+
+		if (!data || error) return [];
+
+		const formattedData: getWorkoutRoutineType[] = data.map((routine: { id: string, created_at: string, user_id: string, title: string, WorkoutSets: WorkoutType[] }) => {
+			const { WorkoutSets, ...rest } = routine;
+			return {
+				...rest,
+				workouts: WorkoutSets
+			};
+		});
+
+		return formattedData;
+	} catch (error) {
+		return null;
+	}
+}
