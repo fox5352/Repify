d52930ecbfa4834a3fe5f5204d02855b6acce68c
fox5352 <christophervos522@gmail.com>
2025-05-26 13:47:49 +0200

    upadted tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,13 @@ module.exports = {
   content: [
     "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   darkMode: "class",
-  plugins: [heroui()],
+  plugins: [heroui(), require("tailwindcss-animate")],
 };
