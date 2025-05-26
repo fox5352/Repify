04b46642c679e1f91372e7a81d72a5d3a2afe548
fox5352 <christophervos522@gmail.com>
2025-05-26 13:48:27 +0200

    added table and updated skeleton components

--- src/components/ui/SkeletonBox.tsx
+++ src/components/ui/SkeletonBox.tsx
@@ -4,13 +4,13 @@ export default function SkeletionBox() {
   return (
     <>
       <div className="flex gap-2">
-        <Skeleton className="h-[125px] basis-2/6 rounded-xl" />
-        <Skeleton className="h-[125px] basis-full rounded-xl" />
+        <Skeleton className="h-[125px] basis-2/6 rounded-xl bg-zinc-200 dark:bg-zinc-800" />
+        <Skeleton className="h-[125px] basis-full rounded-xl bg-zinc-200 dark:bg-zinc-800" />
       </div>
-      <Skeleton className="h-[125px] w-full rounded-xl" />
+      <Skeleton className="h-[125px] w-full rounded-xl bg-zinc-200 dark:bg-zinc-800" />
       <div className="flex flex-row-reverse gap-2">
-        <Skeleton className="h-[125px] basis-2/6 rounded-xl" />
-        <Skeleton className="h-[125px] basis-full rounded-xl" />
+        <Skeleton className="h-[125px] basis-2/6 rounded-xl bg-zinc-200 dark:bg-zinc-800" />
+        <Skeleton className="h-[125px] basis-full rounded-xl bg-zinc-200 dark:bg-zinc-800" />
       </div>
 
     </>
--- src/components/ui/skeletonCard.tsx
+++ src/components/ui/skeletonCard.tsx
@@ -1,12 +1,13 @@
 import { Skeleton } from "@/components/ui/skeleton"
+import { cn } from "@/lib/utils"
 
-export function SkeletonCard() {
+export function SkeletonCard({ className }: { className?: string }) {
   return (
-    <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+    <div className={`flex flex-col w-fit space-y-3 p-1 rounded-md shadow-md dark:shadow-none shadow-stone-200 ${cn(className)}`}>
+      <Skeleton className="h-[125px] min-w-[250px] w-full rounded-xl bg-zinc-200 dark:bg-zinc-800" />
+      <div className="flex flex-col space-y-2 w-full">
+        <Skeleton className="w-4/5 h-4 min-w-[250px] bg-zinc-200 dark:bg-zinc-800" />
+        <Skeleton className="w-4/5 h-4 min-w-[250px] bg-zinc-200 dark:bg-zinc-800" />
       </div>
     </div>
   )
