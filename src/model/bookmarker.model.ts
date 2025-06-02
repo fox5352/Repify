d5050eb042f82dde55a5c03fed34b89e97418dab
fox5352 <christophervos522@gmail.com>
2025-06-02 15:13:41 +0200

    changed bookmarkers to use local db and dexie orm

--- src/model/bookmarker.model.ts
+++ src/model/bookmarker.model.ts
@@ -1,66 +1,80 @@
-import { db } from "./database";
-import { getUser, incrementBookmarkerLength } from "./user.model";
-import type { getWorkoutRoutineType } from "./workoutroutine.model";
+import Dexie from "dexie";
+import type { WorkoutRoutine } from "./workoutroutine.model";
 
-export interface Bookmarker {
-	id: string;
-	user_id: string;
-	workout_routine_id: string;
-	created_at: string;
+const db = new Dexie("bookmarkers");
+db.version(1).stores({ bookmarkers: "++id,_id,title" });
+
+export interface Bookmarker extends WorkoutRoutine {
+	_id: string,
+	saved_at: string;
 }
 
+const bookmarkerTable = db.table<Bookmarker, number>("bookmarkers");
 
-
-export async function createBookMarker(id: string): Promise<boolean> {
+export async function createBookMarker(data: WorkoutRoutine & { _id: string }): Promise<boolean> {
 	try {
-		const user = await getUser();
+		const newBookmarker: Bookmarker = {
+			...data,
+			saved_at: (new Date()).toISOString()
+		};
 
-		if (!user) return false;
+		await bookmarkerTable.add(newBookmarker)
 
-		const { error } = await db.from("Bookmarkers").insert({
-			user_id: user.id,
-			workout_routine_id: id
-		});
-
-		if (error) {
-			return false;
-		}
-
-		await incrementBookmarkerLength();
-
-		return true;
+		return true
 	} catch (error) {
+		console.error("failed to bookmark post");
 		return false;
 	}
 }
 
-export async function getBookmarkers(): Promise<getWorkoutRoutineType[] | null> {
+export async function getBookmarkers(page: number = 1, limit: number = 10): Promise<Bookmarker[] | null> {
 	try {
-		const user = await getUser();
-		if (!user) return null;
+		const result = await bookmarkerTable
+			.orderBy("title")
+			.offset((page - 1) * limit)
+			.limit(limit)
+			.toArray();
 
-		const { data } = await db.from("Bookmarkers").select("*, WorkoutRoutines(*, WorkoutSets(*))").eq("user_id", user.id);
-
-		if (!data) return null;
-
-		const workoutRoutines: getWorkoutRoutineType[] = [];
-
-		for (const bookmark of data) {
-			const { WorkoutRoutines: workoutRoutine } = bookmark;
-
-			const routine: getWorkoutRoutineType = {
-				id: workoutRoutine.id,
-				user_id: bookmark.user_id,
-				created_at: bookmark.created_at,
-				title: workoutRoutine.title,
-				workouts: workoutRoutine.WorkoutSets
-			}
-
-			workoutRoutines.push(routine);
-		}
-
-		return workoutRoutines
+		return result;
 	} catch (error) {
+		console.error("failed to get bookmarkers from local database");
+		return null
+	}
+}
+
+export async function getBookmarkerById(_id: string): Promise<Bookmarker | null> {
+	try {
+		const result = await bookmarkerTable.where("_id").equals(_id).first();
+
+		if (!result) return null;
+
+		return result
+	} catch (error) {
+		console.error("failed to get database :" + _id);
 		return null;
+	}
+}
+
+export async function bookmarkExists(id: string): Promise<boolean> {
+	try {
+		const result = await bookmarkerTable.where("id").equals(id).first();
+
+		if (!result) return false;
+
+		return true;
+	} catch (error) {
+		console.error("failed to validate bookmarker's existence'")
+		return false;
+	}
+}
+
+export async function deleteBookmarker(id: string): Promise<boolean> {
+	try {
+		await bookmarkerTable.delete(id);
+
+		return true;
+	} catch (error) {
+		console.error("failed to remove bookmarker :" + _id);
+		return false;
 	}
 }
