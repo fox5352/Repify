37768a814bf7c4ecd6f1b7a2b04510259400b00b
fox5352 <christophervos522@gmail.com>
2025-06-22 03:19:53 +0200

    fixed notification logo

--- src/hooks/notification.tsx
+++ src/hooks/notification.tsx
@@ -1,19 +1,29 @@
 import { isPermissionGranted, requestPermission, sendNotification, type Options } from "@tauri-apps/plugin-notification";
 
-export async function notification(options: Options) {
+export async function notification(opt: Options) {
   let isPerm: boolean = await isPermissionGranted()
   console.log("isPermissionGranted:" + isPerm);
 
+  let options: Options = {
+    ...opt,
+    icon: "ic_notification",
+    largeIcon: "ic_notification"
+  }
+
+
   if (!isPerm) {
+    console.log("requesting permission");
     let req: NotificationPermission = await requestPermission();
     console.log("requestPermission:" + req);
 
     if (req == "granted") {
+      console.log("granted");
       sendNotification(options);
     }
   }
 
   if (isPerm) {
+    console.log("granted");
     sendNotification(options);
   }
 }
